a5e1f49c70102b0e81aa625f745ed663cb9bb479
Lukas <lukasdrinkwater97@gmail.com>
2024-01-21 13:48:01 +0000

    base structure for popup to test to see if code is working and being placed correctly

---
+++ frontend/src/components/board/Popup.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect, useRef } from "react";
+
+import "./popup.css";
+
+import CharacterSelect from "./CharacterSelectPopup";
+
+function Popup({ cursorPosition }) {
+  const popupRef = useRef(null);
+
+  useEffect(() => {
+    // console.log(cursorPosition);
+  }, [popupRef, cursorPosition]);
+
+  useEffect(() => {
+    const popup = popupRef.current;
+    // console.log(popup);
+    // const rect = popup.getBoundingClientRect();
+    // console.log(rect);
+    popup.style.left = `${cursorPosition.x + 10}px`;
+    popup.style.top = `${cursorPosition.y}px`;
+  }, [cursorPosition]);
+
+  return (
+    <div className="popup" ref={popupRef}>
+      <div className="popupInner">
+        <CharacterSelect />
+      </div>
+    </div>
+  );
+}
+
+export default Popup;
