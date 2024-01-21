f3e91fd694b5a2783a7b29e8bda6d9bea868ecf5
Lukas <lukasdrinkwater97@gmail.com>
2024-01-21 13:46:46 +0000

    initial commit

---
+++ frontend/src/components/board/CharacterSelectPopup.jsx
@@ -0,0 +1,22 @@
+function CharacterSelect() {
+  return (
+    <>
+      <div className="characterSelectPopup">
+        <div className="characterName">
+          <p>Waldo</p>
+        </div>
+        <div className="characterName">
+          <p>Wenda</p>
+        </div>
+        <div className="characterName">
+          <p>Wizard</p>
+        </div>
+        <div className="characterName">
+          <p>Odlaw</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CharacterSelect;
