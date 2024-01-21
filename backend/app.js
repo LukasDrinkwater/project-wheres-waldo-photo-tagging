afce9a37b7553be0377dde62bd6f2742a89c00bc
Lukas <lukasdrinkwater97@gmail.com>
2024-01-21 13:45:41 +0000

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,61 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+
+// Import routes
+const imageRouter = require("./routes/imageRoutes");
+const userRouter = require("./routes/userRoutes");
+
+const app = express();
+
+// Setup mongoose mongoDB connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGODB_STRING;
+
+main().catch((error) => console.log(error));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+console.log(mongoose.connection.readyState);
+
+// Middleware setup
+app.use(logger("dev"));
+app.use(express.json());
+// app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  //cors is needed to allow requests from the React front end
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5000"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(express.urlencoded({ extended: false }));
+
+// Route setup
+app.use("/image", imageRouter);
+app.use("/user", userRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  // res.render("error");
+});
+
+module.exports = app;
