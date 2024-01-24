7e94478defdcff227e6899cea33035b3da926399
Lukas <lukasdrinkwater97@gmail.com>
2024-01-24 09:10:12 +0000

    started working on api post to check character pick

--- frontend/src/components/board/CharacterSelectPopup.jsx
+++ frontend/src/components/board/CharacterSelectPopup.jsx
@@ -1,18 +1,54 @@
-function CharacterSelect() {
+const imageName = "whereswaldo2.png";
+
+function CharacterSelect({ userPixelPercentPosition }) {
+  const stopClickPropagation = (event) => {
+    event.stopPropagation();
+  };
+
+  const handButtonClick = async (e) => {
+    e.preventDefault();
+    const character = e.target.value;
+
+    try {
+      const response = await fetch(
+        // http://localhost:3000/image/whereswaldo2.png
+        `http://localhost:3000/image/${imageName}/characterPick`,
+        {
+          mode: "cors",
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+
+          body: JSON.stringify({
+            character: character,
+            pixelX: userPixelPercentPosition.width,
+            pixelY: userPixelPercentPosition.height,
+          }),
+        }
+      );
+
+      if (response.ok) {
+        // character found do something
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
-      <div className="characterSelectPopup">
+      <div className="characterSelect" onClick={stopClickPropagation}>
         <div className="characterName">
-          <p>Waldo</p>
+          <button onClick={handButtonClick}>Waldo</button>
         </div>
         <div className="characterName">
-          <p>Wenda</p>
+          <button>Wenda</button>
         </div>
         <div className="characterName">
-          <p>Wizard</p>
+          <button>Wizzard</button>
         </div>
         <div className="characterName">
-          <p>Odlaw</p>
+          <button>Odlaw</button>
         </div>
       </div>
     </>
