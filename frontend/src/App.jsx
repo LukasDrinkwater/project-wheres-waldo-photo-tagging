859ed7edd725b144331c4291018809f7675ef2ee
Lukas <lukasdrinkwater97@gmail.com>
2024-01-28 14:00:42 +0000

    tidy up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { createContext } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Homepage from "./components/pages/Homepage";
 
 import "./App.css";
+
+export const CharacterStatus = createContext();
 
 function App() {
   return (
