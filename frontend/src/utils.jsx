57f7ac2306201c0cc3865b6bdeef0b2468214b91
Lukas <lukasdrinkwater97@gmail.com>
2024-01-28 21:13:58 +0000

    tidy up

--- frontend/src/utils.jsx
+++ frontend/src/utils.jsx
@@ -1,5 +1,6 @@
 export function getBaseImageUrl() {
   const backendUrl =
     import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+  console.log(backendUrl);
   return new URL(backendUrl).toString();
 }
