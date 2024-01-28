a542b81da45fc719a7abbcc3e85338bba9393222
Lukas <lukasdrinkwater97@gmail.com>
2024-01-28 14:00:19 +0000

    added GameController component render

--- frontend/src/components/pages/Homepage.jsx
+++ frontend/src/components/pages/Homepage.jsx
@@ -1,16 +1,45 @@
 import ImageComponent from "../board/ImageComponent";
 import CharactersToFind from "../characterInfo/CharactersToFind";
-
-import CharacterSelect from "../board/CharacterSelectPopup";
+import GameController from "../board/GameController";
 
 import "./homepage.css";
+import { useState } from "react";
 
 function Homepage() {
+  const [characterStatus, setCharacterStatus] = useState({
+    waldo: false,
+    wenda: false,
+    wizzard: false,
+    odlaw: false,
+  });
+
+  const [gameStatus, setGameStatus] = useState({
+    started: false,
+    finished: false,
+  });
+
   return (
     <div className="mainContainer">
-      <CharactersToFind />
-
-      <ImageComponent />
+      <CharactersToFind
+        characterStatus={characterStatus}
+        setCharacterStatus={setCharacterStatus}
+      />
+      <GameController
+        characterStatus={characterStatus}
+        setCharacterStatus={setCharacterStatus}
+        gameStatus={gameStatus}
+        setGameStatus={setGameStatus}
+      />
+      {gameStatus.started ? (
+        <ImageComponent
+          characterStatus={characterStatus}
+          setCharacterStatus={setCharacterStatus}
+        />
+      ) : (
+        <div className="imagePlaceholder">
+          <h1>Press Start Game to start</h1>
+        </div>
+      )}
     </div>
   );
 }
