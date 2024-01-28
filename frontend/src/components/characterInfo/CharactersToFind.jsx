ba05f09119ae622271f49c2c926b4b621204e1f9
Lukas <lukasdrinkwater97@gmail.com>
2024-01-28 13:59:19 +0000

    changed img src to point to the backend individual character images. Added conditional classname to add found class

--- frontend/src/components/characterInfo/CharactersToFind.jsx
+++ frontend/src/components/characterInfo/CharactersToFind.jsx
@@ -1,10 +1,39 @@
 import characters from "../../assets/images/characters.png";
 
-function CharactersToFind() {
+function CharactersToFind({ characterStatus, setCharacterStatus }) {
   return (
     <>
       <div className="charactersToFind">
-        <img src={characters} alt="the 4 characters you need to find" />
+        {characterStatus.waldo}
+        <div className={`character ${characterStatus.waldo ? "found" : ""}`}>
+          <img
+            src="http://localhost:3000/images/waldo.png"
+            alt="Drawing of the character Wally"
+          />
+          <p>Waldo</p>
+        </div>
+        <div className={`character ${characterStatus.wenda ? "found" : ""}`}>
+          <img
+            src="http://localhost:3000/images/wenda.png"
+            alt="Drawing of the character Wally"
+          />
+          <p>Wenda</p>
+        </div>
+        <div className={`character ${characterStatus.wizzard ? "found" : ""}`}>
+          <img
+            src="http://localhost:3000/images/wizzard.png"
+            alt="Drawing of the character Wally"
+          />
+          <p>Wizzard</p>
+        </div>
+        <div className={`character ${characterStatus.odlaw ? "found" : ""}`}>
+          <img
+            src="http://localhost:3000/images/odlaw.png"
+            alt="Drawing of the character Wally"
+          />
+          <p>Odlaw</p>
+        </div>
+        {/* <img src={characters} alt="the 4 characters you need to find" /> */}
       </div>
     </>
   );
