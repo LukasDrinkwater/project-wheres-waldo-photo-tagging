1095e7b6283b5be5f1bca95afbcbdb6909df9ae6
Lukas <lukasdrinkwater97@gmail.com>
2024-01-29 11:11:18 +0000

    amended || or to localhost

--- frontend/src/utils.jsx
+++ frontend/src/utils.jsx
@@ -1,7 +1,6 @@
 export function getBaseImageUrl() {
   const backendUrl =
-    import.meta.env.VITE_BACKEND_URL ||
-    "https://project-wheres-waldo.adaptable.app";
+    import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
   console.log(backendUrl);
   return new URL(backendUrl).toString();
 }
